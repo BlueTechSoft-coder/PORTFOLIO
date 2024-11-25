b3b767fde99c5075e0032c944efe7d70a1612fbc
BlueTechSoft-coder <andrewbarasa412@gmail.com>
2024-11-26 00:00:48 +0300

    Add files via upload

---
+++ PORTFOLIO.js
@@ -0,0 +1,18 @@
+﻿let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = set.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
