60f0765ee3e795a4efc523ec20e908dee498820d
BlueTechSoft-coder <andrewbarasa412@gmail.com>
2024-11-29 20:19:50 +0300

    Update PORTFOLIO.js

--- PORTFOLIO.js
+++ PORTFOLIO.js
@@ -1,3 +1,12 @@
+/* DOWNLODING THE CV */
+function downloadFile(fileUrl, fileName) {  
+    const link = document.createElement('a');  
+    link.href = fileUrl;  
+    link.download = fileName;  
+    document.body.appendChild(link);  
+    link.click();  
+    document.body.removeChild(link);
+
 
 /* toggle icon navbar */
 
