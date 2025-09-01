ba9c461b0d8b90ddfa092bd2d30d948b269a6475
andrewbarasa <andrewbarasa412@gmail.com>
2025-09-01 18:16:18 +0300

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,104 @@
+<?php
+
+// connecting to the database
+    $db_server = "localhost";
+    $db_user = "Andrew";
+    $db_pass = "Bensey@05";
+    $db_name = "portfoliodb";
+    $conn = "";
+
+    try{
+        $conn = mysqli_connect($db_server,
+                               $db_user,
+                               $db_pass,
+                               $db_name);
+    }
+    catch(mysqli_sql_exception){
+        // echo "Could not connect! <br>";
+    }
+
+// checking if the database is connected
+    if($conn){
+        // echo "You are connected! <br>";
+    }
+    else{
+        // echo "You are not connected! <br>";
+    }
+
+// checking the inputs
+    if(isset($_POST["submit"])){
+
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+            $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_SPECIAL_CHARS);
+            $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+            $mobile = filter_input(INPUT_POST, "mobile", FILTER_SANITIZE_SPECIAL_CHARS);
+            $subject = filter_input(INPUT_POST, "subject", FILTER_SANITIZE_SPECIAL_CHARS);
+            $message = filter_input(INPUT_POST, "message", FILTER_SANITIZE_SPECIAL_CHARS);
+
+        // inserting data into contacts table
+        $sql = "INSERT INTO contacts(name, email, mobile, subject, message)
+                VALUES('$name','$email','$mobile','$subject','$message')";
+        
+        try{
+            mysqli_query($conn, $sql);
+            // echo "Thank you! <br>";
+        }
+        catch(mysqli_sql_exception){
+            // echo "Try again! <br>";
+        }
+    }
+    }
+
+    // closing the database connection
+    mysqli_close($conn);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+body{
+    min-height: 100vh;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-family:'Poppins', sans-serif;
+    background-color: rgb(1, 1, 51);
+}
+#thank{
+    margin: 0 auto;
+    height: 100%;
+    padding: 70px 50px;
+    background-color: aqua;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 20px;
+    z-index: 999;
+    box-shadow: 0 0 10px 0 black;
+}
+a{
+    text-decoration: none;
+    background-color: rgb(1, 1, 51);
+    color: aqua;
+    padding: 7px;
+    border-radius: 7px;
+}
+    </style>
+</head>
+<body>
+    <div id="thank">
+        <h2>Thank You!</h2>
+        <a href="index.html">Back Home</a>
+    </div>
+</body>
+</html>
---
+++ portfoliodb.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 30, 2025 at 06:21 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `portfoliodb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
+--
+
+CREATE TABLE `contacts` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mobile` varchar(255) NOT NULL,
+  `subject` varchar(25) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `reg_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `contacts`
+--
+
+INSERT INTO `contacts` (`id`, `name`, `email`, `mobile`, `subject`, `message`, `reg_date`) VALUES
+(1, 'Andrew Barasa Wandera', 'bluesofttech1@gmail.com', '0708909399', 'Web Development', 'Hello Andrew! I need a business website!', '2025-08-30 19:20:23'),
+(2, 'Benson Baraka Mubweka', 'bensey035@gmail.com', '0712822291', 'Web Development', 'Hello Andrew! I need a business website!', '2025-08-30 19:20:56');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscribers`
+--
+
+CREATE TABLE `subscribers` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `reg_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `subscribers`
+--
+
+INSERT INTO `subscribers` (`id`, `email`, `reg_date`) VALUES
+(1, 'bluesofttech1@gmail.com', '2025-08-30 19:13:05'),
+(2, 'bensey035@gmail.com', '2025-08-30 19:13:44');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contacts`
+--
+ALTER TABLE `contacts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subscribers`
+--
+ALTER TABLE `subscribers`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contacts`
+--
+ALTER TABLE `contacts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `subscribers`
+--
+ALTER TABLE `subscribers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
